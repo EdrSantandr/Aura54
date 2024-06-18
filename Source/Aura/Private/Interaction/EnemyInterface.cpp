360a02924cd2deeed6363f4e650a7f2008adca65
Eder Santander <eder.santander.123@gmail.com>
2024-06-18 17:56:18 -0500

    Added enemyinterface to handle highligth actor

---
+++ Source/Aura/Private/Interaction/EnemyInterface.cpp
@@ -0,0 +1,6 @@
+// EdrSantander Dev
+
+
+#include "Interaction/EnemyInterface.h"
+
+// Add default functionality here for any IEnemyInterface functions that are not pure virtual.
---
+++ Source/Aura/Public/Interaction/EnemyInterface.h
@@ -0,0 +1,27 @@
+// EdrSantander Dev
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "EnemyInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UEnemyInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class AURA_API IEnemyInterface
+{
+	GENERATED_BODY()
+
+public:
+	virtual void HighlightActor() = 0; //=0 makes a pure virtual function
+	virtual void UnHighlightActor() = 0;
+	
+};

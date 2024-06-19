79147a0e019c32938fc962ff50fd73974d7f2148
Eder Santander <eder.santander.123@gmail.com>
2024-06-19 17:56:27 -0500

    Added widget and widgetcontroller class

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// EdrSantander Dev
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}

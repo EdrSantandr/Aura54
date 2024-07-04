fb2f29f3b73e8221efbc890280dacecc56da94c8
Eder Santander <eder.santander.123@gmail.com>
2024-07-04 09:56:59 -0500

    Working on the functions to Send Data to the server and recieve when it's needed still WIP

--- Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
+++ Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
@@ -3,6 +3,8 @@
 
 #include "AbilitySystem/AbilityTasks/TargetDataUnderMouse.h"
 
+#include "AbilitySystemComponent.h"
+
 UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
 {
 	UTargetDataUnderMouse* MyObj = NewAbilityTask<UTargetDataUnderMouse>(OwningAbility);
@@ -11,8 +13,37 @@ UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGamepl
 
 void UTargetDataUnderMouse::Activate()
 {
+	if (const bool bIsLocallyControlled = Ability->GetCurrentActorInfo()->IsLocallyControlled())
+	{
+		SendMouseCursorData();	
+	}
+	else
+	{
+		//todo: We are on the server, so listen for target data
+		
+	}
+}
+
+void UTargetDataUnderMouse::SendMouseCursorData()
+{
+	// The next line is needed to use AbilitySystemComponent->ScopedPredictionKey
+	FScopedPredictionWindow ScopedPredictionWindow(AbilitySystemComponent.Get());
 	const APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
 	FHitResult CursorHit;
 	PC->GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
-	MouseTargetDataSignature.Broadcast(CursorHit.Location);
+	FGameplayAbilityTargetDataHandle TargetDataHandle;
+	FGameplayAbilityTargetData_SingleTargetHit* Data = new FGameplayAbilityTargetData_SingleTargetHit();
+	Data->HitResult = CursorHit;
+	TargetDataHandle.Add(Data);
+	AbilitySystemComponent->ServerSetReplicatedTargetData(
+		GetAbilitySpecHandle(),
+		GetActivationPredictionKey(),
+		TargetDataHandle,
+		FGameplayTag(),
+		AbilitySystemComponent->ScopedPredictionKey);
+
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		MouseTargetDataSignature.Broadcast(TargetDataHandle);
+	}
 }

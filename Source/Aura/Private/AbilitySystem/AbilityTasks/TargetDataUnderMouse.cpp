f3eccb5151b064d009abd23ff097901671ffd430
Eder Santander <eder.santander.123@gmail.com>
2024-07-04 09:29:08 -0500

    Overwritten Activate on AbilityTask Targetdataundermouse working, but no for multiplayer

---
+++ Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
@@ -0,0 +1,18 @@
+// EdrSantander Dev
+
+
+#include "AbilitySystem/AbilityTasks/TargetDataUnderMouse.h"
+
+UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
+{
+	UTargetDataUnderMouse* MyObj = NewAbilityTask<UTargetDataUnderMouse>(OwningAbility);
+	return MyObj;
+}
+
+void UTargetDataUnderMouse::Activate()
+{
+	const APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
+	FHitResult CursorHit;
+	PC->GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	MouseTargetDataSignature.Broadcast(CursorHit.Location);
+}

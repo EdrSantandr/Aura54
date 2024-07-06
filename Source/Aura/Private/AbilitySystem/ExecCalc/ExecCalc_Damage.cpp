abdd25ca4ab7c4e4f0508a809aaf2cba152f8510
Eder Santander <eder.santander.123@gmail.com>
2024-07-06 11:55:21 -0500

    Created GameplayeffectExecutionCalculation to manage Damage calcs

---
+++ Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -0,0 +1,22 @@
+// EdrSantander Dev
+
+
+#include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
+
+#include "AbilitySystemComponent.h"
+
+UExecCalc_Damage::UExecCalc_Damage()
+{
+}
+
+void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+                                              FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	const UAbilitySystemComponent* SourceAsc = ExecutionParams.GetSourceAbilitySystemComponent();
+	const UAbilitySystemComponent* TargetAsc = ExecutionParams.GetTargetAbilitySystemComponent();
+
+	AActor* SourceAvatar = SourceAsc ? SourceAsc->GetAvatarActor() : nullptr;
+	AActor* TargetAvatar = TargetAsc ? TargetAsc->GetAvatarActor() : nullptr;
+
+	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
+}

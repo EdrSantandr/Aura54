44429e7907e4a9817afc9424e4783e0f8b15e738
Eder Santander <eder.santander.123@gmail.com>
2024-07-07 17:32:16 -0500

    Created custom aura effect context base class

---
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "GameplayEffectTypes.h"
+#include "AuraAbilityTypes.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAuraGameplayEffectContext : public FGameplayEffectContext
+{
+	GENERATED_BODY();
+
+public:
+	bool IsCriticalHit() const { return bIsCriticalHit; }
+	bool IsBlockedHit() const { return bIsBlockedHit; }
+
+	void SetIsCriticalHit(bool bInIsCriticalHit) { bIsCriticalHit = bInIsCriticalHit; }
+	void SetIsBlockedHit(bool bInIsBlockedHit) { bIsBlockedHit = bInIsBlockedHit; }
+	
+	virtual UScriptStruct* GetScriptStruct() const override
+	{
+		return FGameplayEffectContext::StaticStruct();	
+	}
+
+	virtual bool NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess) override;
+protected:
+
+	UPROPERTY()
+	bool bIsCriticalHit = false;
+
+	UPROPERTY()
+	bool bIsBlockedHit = false;
+};

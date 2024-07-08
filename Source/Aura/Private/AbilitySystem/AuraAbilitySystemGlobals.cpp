81d7baa86e551451a8b0c5ab9914d49434a1f55e
Eder Santander <eder.santander.123@gmail.com>
2024-07-08 10:07:18 -0500

    Setup the AuraGameplayEffectContext using defaultgame.ini

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemGlobals.cpp
@@ -0,0 +1,11 @@
+// EdrSantander Dev
+
+
+#include "AbilitySystem/AuraAbilitySystemGlobals.h"
+
+#include "AuraAbilityTypes.h"
+
+FGameplayEffectContext* UAuraAbilitySystemGlobals::AllocGameplayEffectContext() const
+{
+	return new FAuraGameplayEffectContext();
+}
---
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemGlobals.h
@@ -0,0 +1,17 @@
+// EdrSantander Dev
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemGlobals.h"
+#include "AuraAbilitySystemGlobals.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraAbilitySystemGlobals : public UAbilitySystemGlobals
+{
+	GENERATED_BODY()
+	virtual FGameplayEffectContext* AllocGameplayEffectContext() const override;
+};

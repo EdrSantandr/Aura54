a6c4dfcf2760527387cc5b2aa2d0addc09814aef
Eder Santander <eder.santander.123@gmail.com>
2024-07-09 19:21:35 -0500

    EQS system tested for ranged and melee attackers

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -149,8 +149,11 @@ void AAuraPlayerController::AbilityInputTagReleased(FGameplayTag InputTag)
 				{
 					Spline->AddSplinePoint(PointLocation, ESplineCoordinateSpace::World);
 				}
-				CachedDestination = NavPath->PathPoints[NavPath->PathPoints.Num()-1];
-				bAutoRunning = true;
+				if (NavPath->PathPoints.Num()>0)
+				{
+					CachedDestination = NavPath->PathPoints[NavPath->PathPoints.Num()-1];
+					bAutoRunning = true;
+				}
 			}
 		}
 		FollowTime = 0.f;

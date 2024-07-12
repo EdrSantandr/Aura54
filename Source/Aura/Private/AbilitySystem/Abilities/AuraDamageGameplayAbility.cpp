cad7531a1dcee6d1ca80dab44e097ae50c4b31ed
Eder Santander <eder.santander.123@gmail.com>
2024-07-12 18:09:01 -0500

    Setup the combat pouch for ranged attack in the gameplay ability

--- Source/Aura/Private/AbilitySystem/Abilities/AuraDamageGameplayAbility.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraDamageGameplayAbility.cpp
@@ -18,3 +18,13 @@ void UAuraDamageGameplayAbility::CauseDamage(AActor* TargetActor)
 	GetAbilitySystemComponentFromActorInfo()->ApplyGameplayEffectSpecToTarget(*DamageSpecHandle.Data.Get(),
 		UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor));
 }
+
+FTaggedMontage UAuraDamageGameplayAbility::GetRandomTaggedMontage(const TArray<FTaggedMontage>& TaggedMontages) const
+{
+	if(TaggedMontages.Num() > 0)
+	{
+		const int32 Selection = FMath::RandRange(0, TaggedMontages.Num()-1);
+		return TaggedMontages[Selection];
+	}
+	return FTaggedMontage();
+}

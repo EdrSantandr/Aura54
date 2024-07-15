79b34d9c5a774c6d7fa33758b9fdb250324ddb01
Eder Santander <eder.santander.123@gmail.com>
2024-07-15 10:44:03 -0500

    Added dissolve effect to shaman and fixes to multiplayer con client-server

--- Source/Aura/Private/Actor/AuraProjectile.cpp
+++ Source/Aura/Private/Actor/AuraProjectile.cpp
@@ -54,7 +54,7 @@ void AAuraProjectile::Destroyed()
 void AAuraProjectile::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
                                      UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if(DamageEffectSpecHandle.Data.IsValid() && DamageEffectSpecHandle.Data.Get()->GetContext().GetEffectCauser() == OtherActor)
+	if(!DamageEffectSpecHandle.Data.IsValid() || DamageEffectSpecHandle.Data.Get()->GetContext().GetEffectCauser() == OtherActor)
 	{
 		return;
 	}

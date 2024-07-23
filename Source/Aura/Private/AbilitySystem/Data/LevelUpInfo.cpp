94a498bc45d52f7e0cb1a3b01b50022b662d7a2a
Eder Santander <eder.santander.123@gmail.com>
2024-07-23 11:29:15 -0500

    Added levelupinfo dataasset to manage level requirement and spell points

---
+++ Source/Aura/Private/AbilitySystem/Data/LevelUpInfo.cpp
@@ -0,0 +1,26 @@
+// EdrSantandr Dev
+
+
+#include "AbilitySystem/Data/LevelUpInfo.h"
+
+int32 ULevelUpInfo::FindLevelForExp(int32 Exp)
+{
+	int32 Level = 1;
+	bool bSearching = true;
+	while (bSearching)
+	{
+		// Levelupinformation [1] = Level 1 information
+		// Levelupinformation [2] = Level 1 information
+		if (LevelUpInformation.Num() -1 <= Level) return Level;
+
+		if(Exp >= LevelUpInformation[Level].LevelUpRequirement)
+		{
+			++Level;
+		}
+		else
+		{
+			bSearching=false;	
+		}
+	}
+	return Level;
+}
---
+++ Source/Aura/Public/AbilitySystem/Data/LevelUpInfo.h
@@ -0,0 +1,38 @@
+// EdrSantandr Dev
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "LevelUpInfo.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAuraLevelUpInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 LevelUpRequirement = 0;
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 AttributePointAward = 1;
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 SpellPointAward = 1;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API ULevelUpInfo : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly)
+	TArray<FAuraLevelUpInfo> LevelUpInformation;
+
+	int32 FindLevelForExp(int32 Exp);
+	
+};

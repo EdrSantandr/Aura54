47922f33d57209ba9c59c604d6dc2632aa9bac8f
Eder Santander <eder.santander.123@gmail.com>
2024-07-27 09:46:01 -0500

    Added status and showing them on spellmenu

--- Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -34,6 +34,7 @@ void UAuraWidgetController::BroadcastAbilityInfo()
 		{
 			FAuraAbilityInfo Info = AbilityInfo->FindAbilityInfoForTag(GetAuraASC()->GetAbilityTagFromSpec(AbilitySpec));
 			Info.InputTag = GetAuraASC()->GetInputTagFromSpec(AbilitySpec);
+			Info.StatusTag = GetAuraASC()->GetStatusFromSpec(AbilitySpec);
 			AbilityInfoDelegate.Broadcast(Info);
 		}
 	);

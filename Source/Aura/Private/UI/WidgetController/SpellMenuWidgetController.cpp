432fed532db9e18e717b1da51ca128282fdc92d1
Eder Santander <eder.santander.123@gmail.com>
2024-07-28 11:52:19 -0500

    Clear the description on spellmenu after leveling up and minor fixes

--- Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
@@ -90,6 +90,13 @@ void USpellMenuWidgetController::SpendPointButtonPressed()
 	}
 }
 
+void USpellMenuWidgetController::GlobeDeselect()
+{
+	SelectedAbility.Ability = FAuraGameplayTags::Get().Abilities_None;
+	SelectedAbility.Status = FAuraGameplayTags::Get().Abilities_Status_Locked;
+	SpellGlobeSelectedDelegate.Broadcast(false, false, FString(), FString());
+}
+
 void USpellMenuWidgetController::ShouldEnableButtons(const FGameplayTag& AbilityStatus, int32 SpellPoints, bool& bShouldEnableSpellPointButton, bool& bShouldEnableEquipButton)
 {
 	const FAuraGameplayTags AuraGameplayTags = FAuraGameplayTags::Get();

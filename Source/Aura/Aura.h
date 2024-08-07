7969db25addf8f8c11c9e6572512ee01ae8da270
Eder Santander <eder.santander.123@gmail.com>
2024-08-07 17:47:32 -0500

    Added new constants for Hightlight color

--- Source/Aura/Aura.h
+++ Source/Aura/Aura.h
@@ -5,6 +5,8 @@
 #include "CoreMinimal.h"
 
 #define CUSTOM_DEPTH_RED 250
+#define CUSTOM_DEPTH_BLUE 251
+#define CUSTOM_DEPTH_TAN 252
 #define ECC_PROJECTILE ECollisionChannel::ECC_GameTraceChannel1
 #define ECC_TARGET ECollisionChannel::ECC_GameTraceChannel2
 #define ECC_EXCLUDEPLAYERS ECollisionChannel::ECC_GameTraceChannel3

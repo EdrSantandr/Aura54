4858208dc5e1c2d476d41613261398cd9f583d88
Eder Santander <eder.santander.123@gmail.com>
2024-09-24 18:03:44 -0500

    LIRIA: Reparent pushback ability

---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraPushback.h
@@ -0,0 +1,17 @@
+// EdrSantandr Dev
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraDamageGameplayAbility.h"
+#include "AuraPushback.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraPushback : public UAuraDamageGameplayAbility
+{
+	GENERATED_BODY()
+	
+};

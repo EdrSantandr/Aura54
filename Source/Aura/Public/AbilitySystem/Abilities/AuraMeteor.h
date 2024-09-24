a4b1dfe45410b2b508b4bfe1e18566bb96ab646f
Eder Santander <eder.santander.123@gmail.com>
2024-09-24 17:51:40 -0500

    LIRIA: meteor ability reparent class. Still need to overwrite the descriptions

---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraMeteor.h
@@ -0,0 +1,17 @@
+// EdrSantandr Dev
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraFirebolt.h"
+#include "AuraMeteor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraMeteor : public UAuraFirebolt
+{
+	GENERATED_BODY()
+	
+};

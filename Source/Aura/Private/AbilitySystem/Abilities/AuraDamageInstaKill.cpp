33dc85e236cd3af22c707d3f8b6b6859aa4eefa6
Eder Santander <eder.santander.123@gmail.com>
2024-09-25 08:40:43 -0500

    LIRIA: Instakill description updated and cooldown scalable float setup

--- Source/Aura/Private/AbilitySystem/Abilities/AuraDamageInstaKill.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraDamageInstaKill.cpp
@@ -3,3 +3,30 @@
 
 #include "AbilitySystem/Abilities/AuraDamageInstaKill.h"
 
+FString UAuraDamageInstaKill::GetDescription(int32 Level)
+{
+	const float ManaCost = FMath::Abs(GetManaCost(Level));
+	const float Cooldown = GetCooldown(Level);
+	return FString::Printf(TEXT(
+			"<Title>DIE</>\n\n"
+			"<Small>Level: </><Level>%d</>\n"
+			"<Small>ManaCost: </><ManaCost>%.1f</>\n"
+			"<Small>Cooldown: </><Cooldown>%.1f</>\n\n"
+			"<Default>Fires a fast projectile that kills enemy on impact. </>"
+			),
+			Level, ManaCost, Cooldown);
+}
+
+FString UAuraDamageInstaKill::GetNextLevelDescription(int32 Level)
+{
+	const float ManaCost = FMath::Abs(GetManaCost(Level));
+	const float Cooldown = GetCooldown(Level);
+	return FString::Printf(TEXT(
+			"<Title>NEXT LEVEL</>\n\n"
+			"<Small>Level: </><Level>%d</>\n"
+			"<Small>ManaCost: </><ManaCost>%.1f</>\n"
+			"<Small>Cooldown: </><Cooldown>%.1f</>\n\n"
+			"<Default>Fires a fast projectile that kills enemy on impact. </>"
+			),
+			Level, ManaCost, Cooldown);
+}

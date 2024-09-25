6f8363395880367dc8d490c65a735a9e32eacc18
Eder Santander <eder.santander.123@gmail.com>
2024-09-25 08:59:35 -0500

    LIRIA: Pushback ability updated with descriptions, scaled damage and cooldowns

--- Source/Aura/Private/AbilitySystem/Abilities/AuraPushback.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraPushback.cpp
@@ -3,3 +3,34 @@
 
 #include "AbilitySystem/Abilities/AuraPushback.h"
 
+FString UAuraPushback::GetDescription(int32 Level)
+{
+	const int32 ScaledDamage = Damage.GetValueAtLevel(Level);
+	const float ManaCost = FMath::Abs(GetManaCost(Level));
+	const float Cooldown = GetCooldown(Level);
+	return FString::Printf(TEXT(
+			"<Title>PUSHBACK</>\n\n"
+			"<Small>Level: </><Level>%d</>\n"
+			"<Small>ManaCost: </><ManaCost>%.1f</>\n"
+			"<Small>Cooldown: </><Cooldown>%.1f</>\n\n"
+			"<Default>Creates wind shield dealing </>"
+			"<Damage>%d</><Default> radial damage around Liria throwing away the enemies.</>"
+			),
+			Level, ManaCost, Cooldown, ScaledDamage);
+}
+
+FString UAuraPushback::GetNextLevelDescription(int32 Level)
+{
+	const int32 ScaledDamage = Damage.GetValueAtLevel(Level);
+	const float ManaCost = FMath::Abs(GetManaCost(Level));
+	const float Cooldown = GetCooldown(Level);
+	return FString::Printf(TEXT(
+			"<Title>NEXT LEVEL</>\n\n"
+			"<Small>Level: </><Level>%d</>\n"
+			"<Small>ManaCost: </><ManaCost>%.1f</>\n"
+			"<Small>Cooldown: </><Cooldown>%.1f</>\n\n"
+			"<Default>Creates wind shield dealing </>"
+			"<Damage>%d</><Default> radial damage around Liria throwing away the enemies.</>"
+			),
+			Level, ManaCost, Cooldown, ScaledDamage);
+}
